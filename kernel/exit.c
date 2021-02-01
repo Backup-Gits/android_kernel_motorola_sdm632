344564ff0abc429672df7bfa4ff56352c83c21b0
Thomas Gleixner <tglx@linutronix.de>
2021-02-01 10:01:38 +0000

    futex: Mark the begin of futex exit explicitly
    commit 18f694385c4fd77a09851fd301236746ca83f3cb upstream.
    
    Instead of relying on PF_EXITING use an explicit state for the futex exit
    and set it in the futex exit function. This moves the smp barrier and the
    lock/unlock serialization into the futex code.
    
    As with the DEAD state this is restricted to the exit path as exec
    continues to use the same task struct.
    
    This allows to simplify that logic in a next step.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Ingo Molnar <mingo@kernel.org>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20191106224556.539409004@linutronix.de
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/exit.c
+++ kernel/exit.c
@@ -804,6 +804,7 @@ void __noreturn do_exit(long code)
 #else
 		pr_alert("Fixing recursive fault but reboot is needed!\n");
 
+
 #endif
 		/*
 		 * We can do this unlocked here. The futex code uses
@@ -818,11 +819,15 @@ void __noreturn do_exit(long code)
 
 		futex_exit_done(tsk);
 >>>>>>> 2c116895783b... futex: Replace PF_EXITPIDONE with a state
+
+		futex_exit_recursive(tsk);
+>>>>>>> 32d782808b84... futex: Mark the begin of futex exit explicitly
 		set_current_state(TASK_UNINTERRUPTIBLE);
 		schedule();
 	}
 
 	exit_signals(tsk);  /* sets PF_EXITING */
+
 
 	sched_exit(tsk);
 	schedtune_exit_task(tsk);
@@ -841,6 +846,8 @@ void __noreturn do_exit(long code)
 	 * mm_release() -> exit_pi_state_list().
 	 */
 	raw_spin_unlock_wait(&tsk->pi_lock);
+
+>>>>>>> 32d782808b84... futex: Mark the begin of futex exit explicitly
 
 	/* sync mm's RSS info before statistics gathering */
 	if (tsk->mm)
